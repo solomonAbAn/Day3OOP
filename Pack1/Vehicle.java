34c4434a3e7df2086d1f0caa46b4145ec8968ed7
Sleiman Abou antoun <sleimen.aa@gmail.com>
2023-12-20 16:20:52 -0500

    first commit

---
+++ Pack1/Vehicle.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+public class Vehicle {
+//	The abstract keyword is a non-access modifier, used for classes and methods: .
+//	Abstract class: is a restricted class that cannot be used to create objects 
+//	(to access it, it must be inherited from another class). 
+//	Abstract method: can only be used in an abstract class, and it does not have a body.
+	
+	
+	
+	
+}
